9a32c598d99817b4b566f291e2a4461d30f47924
iampopov <ivantsar@gmail.com>
2020-02-25 21:51:27 -0700

    built out header using router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Header />
+      
       
     </div>
+    </Router>
   );
 }
 
