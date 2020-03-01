90662427abd598a72fae37f818f65edd3f7bd9e5
iampopov <ivantsar@gmail.com>
2020-03-01 16:46:28 -0700

    Updates

---
+++ precache-manifest.6f5a2ff78f8952a0b8f6590b4630b4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c6a0e98e7c2279e7fa051a6c17355a",
+    "url": "/reactprofile/index.html"
+  },
+  {
+    "revision": "b547b04fd47171ade0b0",
+    "url": "/reactprofile/static/js/2.888b9cc4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/reactprofile/static/js/2.888b9cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540bc13264480c86f21b",
+    "url": "/reactprofile/static/js/main.af776402.chunk.js"
+  },
+  {
+    "revision": "94a28c3dad7d9c94b127",
+    "url": "/reactprofile/static/js/runtime-main.a1d41f06.js"
+  }
+]);
